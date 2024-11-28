b3d44bb2e3d54138d0293c6bd12de6a000e1b069
Inkflow <tomcucherosset@hotmail.fr>
2024-11-28 11:29:23 +0100

    Changement dans CreerTraversees

--- SQL/dumpBDD.sql
+++ SQL/dumpBDD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 28 nov. 2024 à 09:29
+-- Généré le : jeu. 28 nov. 2024 à 11:28
 -- Version du serveur : 10.4.24-MariaDB
 -- Version de PHP : 7.4.29
 
@@ -40,8 +40,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`idAdmin`, `nom`, `prenom`, `email`, `password`) VALUES
-(1, 'FAUCQUENOY', 'Esteban', 'estebanfcq@gmail.com', '$2y$10$obn3uxGX14fvYfYeg6giR.XlvTEQolbpQoVQYQebwCVhXB5./8vYm'), -- MdP : Osu59
-(2, 'CUCHEROSSET', 'Tom', 'tomcucherosset@hotmail.fr', '$2y$10$wC4oh0Gd/SMtKq6JYb4YguOXuEG7ffwUAjwa2LCpHtogvCfaOZz9.'); -- MdP : Toto59116
+(1, 'FAUCQUENOY', 'Esteban', 'estebanfcq@gmail.com', '$2y$10$obn3uxGX14fvYfYeg6giR.XlvTEQolbpQoVQYQebwCVhXB5./8vYm'),
+(2, 'CUCHEROSSET', 'Tom', 'tomcucherosset@hotmail.fr', '$2y$10$wC4oh0Gd/SMtKq6JYb4YguOXuEG7ffwUAjwa2LCpHtogvCfaOZz9.');
 
 -- --------------------------------------------------------
 
@@ -3471,7 +3471,7 @@ CREATE DEFINER=`root`@`localhost` EVENT `CreerTraversees` ON SCHEDULE EVERY 1 HO
     VALUES (
         DATE_ADD(CURRENT_DATE, INTERVAL FLOOR(RAND() * 30) DAY), -- Date aléatoire dans les 30 prochains jours
         ADDTIME(CURRENT_TIME, SEC_TO_TIME(FLOOR(RAND() * 86400))), -- Heure aléatoire dans la journée
-        FLOOR(1 + RAND() * 40), -- idBat aléatoire entre 1 et 40
+        FLOOR(1 + RAND() * 20), -- idBat aléatoire entre 1 et 20
         FLOOR(1 + RAND() * 126) -- code aléatoire entre 1 et 126
     );
 END$$
